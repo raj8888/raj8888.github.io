21d4b67b985d7a711a47793b62513099b4fb4d09
raj8888 <jadhavrj8877@gmail.com>
2023-03-18 18:02:46 +0530

    change resume open tab

--- index.js
+++ index.js
@@ -60,5 +60,5 @@ document.getElementById('resume-link-1').addEventListener('click',()=>{
 })
 
 document.getElementById('resume-link-2').addEventListener('click',()=>{
-    location.assign("https://drive.google.com/file/d/1B5soRMn7Q4wVzXpnbz3P4VbqDkI-hd_Z/view?usp=share_link","_blank")
+    window.open("https://drive.google.com/file/d/1B5soRMn7Q4wVzXpnbz3P4VbqDkI-hd_Z/view?usp=share_link","_blank")
 })
